8ebb9915d25653199bb49e0dc98994a28bd7d673
Kurtis <kurtismelkisoft@gmail.com>
2024-06-19 23:47:58 +0000

    create_project
    C'est le premier commit du projet

---
+++ back/src/test/java/com/hackaton_back/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton_back.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
